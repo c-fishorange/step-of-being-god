5060013089672a06b6e962c93131ad25c37567a1
cyc-autorun <cyc1828131288@163.com>
2025-09-15 22:52:57 +0800

    Update order_list.cpp

--- List/order_list.cpp
+++ List/order_list.cpp
@@ -13,7 +13,31 @@ struct list
 {
   std::unique_ptr<list_node[]> number_list; 
   int length;
+  int BankCount = 0;
+  int BankNum[MAXSIZE];
 };
+
+void addBank(list &L,int local)
+{
+    L.BankNum[L.BankCount] = local; 
+    L.BankCount ++;
+}
+
+void DelBank(list &L,int local)
+{
+  if(L.BankCount == 1) L.BankCount--;
+  for(int i = 0;i<L.BankCount;i++)
+  {
+    if(L.BankNum[i] == local) 
+    {
+      for(int j = i;j<L.BankCount-1;j++)
+      {
+        L.BankNum[j] = L.BankNum[j + 1];
+      }
+      L.BankCount--;
+    }
+  }
+}
 
 list Init_list() //初始化列表这里用了智能指针
 {
@@ -22,6 +46,26 @@ list Init_list() //初始化列表这里用了智能指针
   if(!L.number_list) exit(1);
   L.length = 0;
   return L;
+}
+
+void del_list(list &L,int location)
+{
+  if(location == L.length) 
+  {
+    // L.number_list[L.length].number = 0;
+    // std::strcpy(L.number_list[L.length].name,"");
+    L.length--; //想了一下直接--就可以了
+  }
+  else if(location<=0 || location>L.length)
+  { 
+    std::cout << "无效位置！" << std::endl; 
+    exit(1);
+  }
+  else
+  {
+    L.number_list[location].number = 0;
+    std::strcpy(L.number_list[location].name,"虚位以待");//这边的长度是不变的
+  }
 }
 
 void add_list(list &L,char name[20],int number)
